0d6374b1df92ce433dfa95959c2de692e6bf2b45
v4hn <me@v4hn.de>
2025-11-22 00:42:26 +0100

    image_publisher needs consistently longer than 1s in some situations
    most likely related to the system load, but not clearly identified.

--- cu_sawyer_bringup/scriptpanel/cuify.sh
+++ cu_sawyer_bringup/scriptpanel/cuify.sh
@@ -2,5 +2,5 @@
 
 rosrun image_publisher image_publisher _filename:=`rospack find cu_sawyer_bringup`/data/CU_Logo_RGB_Color_On_White.png __name:=cu_img_publisher cu_img_publisher/image_raw:=/robot/head_display &
 
-sleep 1
+sleep 3
 rosnode kill /cu_img_publisher
