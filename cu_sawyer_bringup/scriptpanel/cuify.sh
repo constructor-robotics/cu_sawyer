61a9f9befdcbe81320de9fe6d33f9aeca2a9cb03
v4hn <me@v4hn.de>
2025-12-12 14:18:18 +0100

    keep publishing cuify image

--- cu_sawyer_bringup/scriptpanel/cuify.sh
+++ cu_sawyer_bringup/scriptpanel/cuify.sh
@@ -2,5 +2,7 @@
 
 rosrun image_publisher image_publisher _filename:=`rospack find cu_sawyer_bringup`/data/CU_Logo_RGB_Color_On_White.png __name:=cu_img_publisher cu_img_publisher/image_raw:=/robot/head_display &
 
-sleep 3
-rosnode kill /cu_img_publisher
+# no need to kill the node, the next image publisher will overwrite it
+# as long as it uses the same name
+#sleep 3
+#rosnode kill /cu_img_publisher
