21a41e7b279537cde06c576535600337e574d4d6
v4hn <me@v4hn.de>
2026-01-12 14:56:40 +0100

    fixup move-to scripts with moveit_by_name
    they kept running in the background...

--- cu_sawyer_bringup/scriptpanel/move-to-zero.sh
+++ cu_sawyer_bringup/scriptpanel/move-to-zero.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec rostopic pub /moveit_by_name moveit_by_name/Command "{'group':'arm','target': 'extended'}" 
+exec rostopic pub -1 /moveit_by_name moveit_by_name/Command "{'group':'arm','target': 'extended'}" 
